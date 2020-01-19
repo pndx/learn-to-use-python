7ee4b8fd4b1f55f9cd5f168721660f3b25b59312
XiaoyiWong <wangxiaoyipndx@gmail.com>
2020-01-20 02:26:11 +0800

    Add second file

---
+++ test4.py
@@ -0,0 +1,20 @@
+my_list = [100, 55, 67, 33, 101, 23, 43, 100, 23]
+look_for = 44
+
+for x in my_list:
+    if x == look_for:
+        print("found", look_for)
+        break
+else:
+    print("Not found", look_for)
+
+list1 = [1, 2, 3, 4, 5, 6]
+list2 = [2, 4, 6]
+
+for x in list1:
+    for y in list2:
+        if x == y:
+            print(x, "is in both lists")
+            break
+    else:
+        print(x, "is not in list1")
