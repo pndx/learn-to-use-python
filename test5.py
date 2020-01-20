4cc6e4618e0a1cb5e283ef495c0cb5813860ad26
XiaoyiWong <wangxiaoyipndx@gmail.com>
2020-01-21 00:26:20 +0800

    Update File five

---
+++ test5.py
@@ -0,0 +1,23 @@
+my_list = ["apples", "pears", "oranges", "fruits"]
+
+# way 1
+count = 0
+for element in my_list:
+    print(element)
+    if count == 1:
+        print("count is 1")
+
+    count += 1
+
+
+# way 2
+for x in range(len(my_list)):
+    print(my_list[x])
+    if x == 1:
+        print("x is 1")
+
+# better
+for x, element in enumerate(my_list):
+    print(x, element)
+    if x == 1:
+        print("x is 1")
