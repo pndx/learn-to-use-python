befdbe2c4f90b2d5cba16f501a73ee24ced19f1e
XiaoyiWong <wangxiaoyipndx@gmail.com>
2020-01-21 01:00:50 +0800

    Add file 10

---
+++ test10.py
@@ -0,0 +1,21 @@
+x = [1, 2, 3, 4]
+y = [1, 1, 3, 7, 9]
+z = ["one", "two"]
+
+for i in range(min(len(x), len(y))):
+    if x[i] == y[i]:
+        print("Same")
+    else:
+        print('Not Same')
+
+
+# Zip function
+print(list(zip(x, y)))
+print(list(zip(y, x)))
+print(list(zip(y, x, z)))
+
+for i, j in zip(x, y):
+    if i == j:
+        print("Same")
+    else:
+        print("Not Same")
