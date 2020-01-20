20aa2f584170ab1bc3a67af8d11fb985dcbff032
XiaoyiWong <wangxiaoyipndx@gmail.com>
2020-01-21 01:07:09 +0800

    Add file 11

---
+++ test11.py
@@ -0,0 +1,19 @@
+# print(dir(int))
+# print(dir(str))
+# print(dir(bool))
+# print(help(int))
+
+
+class MyObject:
+    """
+    This is a custom obj for testing
+    """
+
+    def __init__(self, name):
+        """
+        This is inits
+        """
+        self.name = name
+
+
+print(help(MyObject))
