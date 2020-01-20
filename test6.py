560315e0e370ba11135c803d4eb0552413efde87
XiaoyiWong <wangxiaoyipndx@gmail.com>
2020-01-21 00:29:40 +0800

    Add file 6

---
+++ test6.py
@@ -0,0 +1,6 @@
+name = "tom"
+age = 20
+
+ex1 = "my name is " + name + " and I am " + str(age) + " years old"
+ex2 = "my name is {0} and I am {1} years old".format(name, age)
+ex3 = "my name is %s and I am %i years old" %(name, age)
