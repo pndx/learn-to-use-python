f0c60289fe122ca26040ff0ca6df7b3b6aad9356
XiaoyiWong <wangxiaoyipndx@gmail.com>
2020-01-21 00:45:38 +0800

    Add file 8

---
+++ test8.py
@@ -0,0 +1,31 @@
+x = [i for i in range(10)]
+
+print(x)
+
+x = [i for i in range(10) if i % 2 == 0]
+
+print(x)
+
+x = [(i, y) for i in range(10) for y in range(20)]
+
+print(x)
+
+x = [[] for i in range(10)]
+
+print(x)
+
+x = [[0 for i in range(5)] for j in range(10)]
+
+print(x)
+
+x = [[True for i in range(5)] for j in range(10)]
+
+print(x)
+
+x = [[[] for i in range(5)] for j in range(10)]
+
+print(x)
+
+x = [[0 for i in range(5)] for j in range(5)]
+
+print(x)
